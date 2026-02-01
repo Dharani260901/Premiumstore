6ac325714b954ac2c709087223f96496e34721c2
Dharani Malairaj <104492210+Dharani260901@users.noreply.github.com>
2026-02-01 22:27:17 +0530

    Delete ui/premiumstore/src directory

--- ui/premiumstore/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-white py-6 text-center text-sm text-gray-500 border-t">
-      © {new Date().getFullYear()} PremiumStore. All rights reserved.
-    </footer>
-  );
-}
